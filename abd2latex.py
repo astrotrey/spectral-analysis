bb4eddd8995d875146ab0abcc32bc5de0eb6424e
Claude "Trey" Mack <31007260+astrotrey@users.noreply.github.com>
2017-08-16 14:05:33 +0200

    Update abd2latex.py
    Cleaned up the comments and spacing between different sections a little bit

--- abd2latex.py
+++ abd2latex.py
@@ -5,7 +5,7 @@
 #
 """
 Convert moog abd output files to latex-formatted
-table rows that can be copied and pasted into latex
+table rows that can be copied and pasted into a latex
 table environment
 """
 
@@ -16,7 +16,6 @@ table environment
 #--###################################################
 import numpy as np
 import sys
-
 
 #-- Print the command and arguments to the screen
 #--
@@ -29,27 +28,24 @@ ifile02 = sys.argv[2]
 ofile01 = sys.argv[3]
 
 
+
 #--###################################################
 #-- Read in the data from the (modified) moog abd files
 #--
 #--###################################################
 
-
 #-- Read in the data from first input file
 #--
 wave01, elem01, ep01, loggf01, ew01, logN01 = np.loadtxt(ifile01, usecols=(0,1,2,3,4,6), unpack=True)
-#print wave01, elem01
 
-#-- Read in the wavelength from second input file as way to check that the
-#-- the data arrays in the two files have the same dimension
+#-- Read in the wavelength from second input
 #--
 wave02, ew02, logN02 = np.loadtxt(ifile02, usecols=(0,4,6), unpack=True)
-print wave02, ew02, logN02
 
 
 
 #--###################################################
-#-- Write the data to ofile01 in latex-table format
+#-- Write the data to the output file in latex-table format
 #--
 #--###################################################
 
@@ -66,13 +62,6 @@ while (i <= len(wave01)-1):
     #-- Determine the text string for
     #-- the variable elem_name given the
     #-- numerical value of elem01[i]
-    #-- (Next: try to use a dictionary,
-    #--        case statement, or sequence
-    #--        of elif statements to accomplish
-    #--        this in a more general way for all
-    #--        chemical species. Elif or case statement
-    #--        implementation would probably be better
-    #--        handled by a pre-defined function.)
     #--
     if (elem01[i] !=  26.1):
         elem_name='Fe'
@@ -83,7 +72,7 @@ while (i <= len(wave01)-1):
 
 
     #-- Define the format and print to the
-    #-- output text file row-by-row
+    #-- output file row-by-row
     #-
     fid=open(ofile01,'a')
     form='\\ion{%2s}{%1s} & %8.2f & %5.2f & %7.3f & %5.1f & %5.2f & %5.1f & %5.2f \\\\ \n'
