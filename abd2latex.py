af21ee2c3469af4a15600eefdf7e79446b95784b
Claude "Trey" Mack <31007260+astrotrey@users.noreply.github.com>
2017-08-31 11:39:23 +0200

    Significant revision of abd2latex.py
    Significantly revised abd2latex.py based on code review with Kevin.
    (1) Defined two functions, get_elem_name() and reformat_files(), to handle converting the element's numerical ID to a text string, and to process and reformat the input files, respectively.
    (2) Imported argparse to process the arguments and provide usage help for the user.
    (3) To make it more unix-like, the output is now by default printed to stdout, unless an output file is specified with the -o option.
    (4) Used the "with ___ as ___:" context to write the reformatted data to the output file. This method could be very memory inefficient for large files, but the text files being reformatted are never going to be that large.
    (5) Attempted to make the variable names more intuitive and straightforward.

--- abd2latex.py
+++ abd2latex.py
@@ -1,87 +1,97 @@
 #!/usr/bin/env python
-#
-#
-#
-#
 """
-Convert moog abd output files to latex-formatted
-table rows that can be copied and pasted into a latex
-table environment
+Convert two data files to a format that can be inserted into a 
+LaTeX table. 
 """
 
-
-#--###################################################
-#-- Import modules and parse the input arguments
-#--
-#--###################################################
 import numpy as np
 import sys
-
-#-- Print the command and arguments to the screen
-#--
-print(sys.argv)
-
-#-- Assign the input arguments to variables
-#--
-ifile01 = sys.argv[1]
-ifile02 = sys.argv[2]
-ofile01 = sys.argv[3]
+import argparse
 
 
+def get_elem_name(elem_number):
+    """Converts a floating point number representing an element to a string name.
+      
+    The integer component of the number should be the element's atomic number,
+    and the decimal component should be its ionized state (i.e, 1 = singly ionized,
+    2 = doubly ionized, etc.)
+    """
 
-#--###################################################
-#-- Read in the data from the (modified) moog abd files
-#--
-#--###################################################
-
-#-- Read in the data from first input file
-#--
-wave01, elem01, ep01, loggf01, ew01, logN01 = np.loadtxt(ifile01, usecols=(0,1,2,3,4,6), unpack=True)
-
-#-- Read in the wavelength from second input
-#--
-wave02, ew02, logN02 = np.loadtxt(ifile02, usecols=(0,4,6), unpack=True)
-
-
-
-#--###################################################
-#-- Write the data to the output file in latex-table format
-#--
-#--###################################################
-
-#-- Initialize variables for the loop,
-#-- and open the output file for writing,
-#-- so that any pre-existing version of the
-#-- file is deleted
-#--
-i=0
-fid=open(ofile01,'w')
-
-while (i <= len(wave01)-1):
-
-    #-- Determine the text string for
-    #-- the variable elem_name given the
-    #-- numerical value of elem01[i]
-    #--
-    if (elem01[i] !=  26.1):
+    # Currently supports only neutral and singly ionized iron
+    if (elem_number != 26.1):
         elem_name='Fe'
         elem_ion='I'
     else:
         elem_name='Fe'
         elem_ion='II'
+    return elem_name, elem_ion
 
 
-    #-- Define the format and print to the
-    #-- output file row-by-row
-    #-
-    fid=open(ofile01,'a')
-    form='\\ion{%2s}{%1s} & %8.2f & %5.2f & %7.3f & %5.1f & %5.2f & %5.1f & %5.2f \\\\ \n'
-    fid.write(form % (elem_name,elem_ion,wave01[i],ep01[i],loggf01[i],ew01[i],logN01[i],ew02[i],logN02[i]))
+def reformat_files(datafile1_path, datafile2_path, output_path):
+    """Converts data from two files into a LaTeX table format.
 
-    i=i+1
+    The data files should be modified from the raw output, i.e.,
+    all rows with non-numerical data should be commented with a '#'.
+    For the variable names:
+         wave = wavelength
+         elem = floating point numerical representation of the element
+         ep = excitation potential for the transition
+         loggf = log of the oscillator strength for the transition
+         ew = equivalent width of the spectral line
+         logN = log of the abundance of the element
+    """
+
+    data1_wave, data1_elem, data1_ep, data1_loggf, data1_ew, data1_logN = \
+               np.loadtxt(datafile1_path, 
+                          usecols=(0,1,2,3,4,6), 
+                          unpack=True)
+
+    # For datafile2, the values for wave, elem, ep, and loggf should
+    # be identical.
+    data2_ew, data2_logN = \
+               np.loadtxt(datafile2_path,
+                          usecols=(4,6),
+                          unpack=True)
+
+    latex_table_format = '\\ion{%2s}{%1s} & %8.2f & %5.2f & %7.3f & %5.1f & %5.2f & %5.1f & %5.2f \\\\'
+
+    out_lines = []
+    for ix, wave_item in enumerate(data1_wave):
+        elem_name, elem_ion = get_elem_name(data1_elem[ix])
+        out_lines.append(latex_table_format % (elem_name,
+                                               elem_ion,
+                                               wave_item,
+                                               data1_ep[ix],
+                                               data1_loggf[ix],
+                                               data1_ew[ix],
+                                               data1_logN[ix],
+                                               data2_ew[ix],
+                                               data2_logN[ix]))
+    outtext = '\n'.join(out_lines)
+    if output_path == sys.stdout:
+       #- add a newline character to the end of the outttext
+       #- string to make the terminal output nicer
+       output_path.write(outtext+'\n')
+    else:
+       with open(output_path, 'w') as f:
+	    f.write(outtext)
 
 
-
-
-
-### END OF PROGRAM
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data1_filepath',
+                         type=str,
+                         help='Path of the input file for the 1st star.')
+    parser.add_argument('data2_filepath',
+                         type=str,
+                         help='Path of the input file for the 2nd star '
+                              '(usu. a reference star like the Sun).')
+    parser.add_argument('-o', '--output_path',
+                         type=str,
+                         default=sys.stdout,
+                         help='Path for the destination file. If no path '
+                              'is specified, output is written to stdout. '
+                              'Note that if a file exists at this path, '
+                              'it will be overwritten.')
+    args = parser.parse_args()
+    reformat_files(args.data1_filepath, args.data2_filepath, args.output_path)
